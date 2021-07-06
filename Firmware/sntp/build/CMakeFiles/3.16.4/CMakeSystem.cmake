f71135eb913b49b30a317861bebdc69a8f1c2976
JosueAGtz <86976466+JosueAGtz@users.noreply.github.com>
2021-07-06 15:01:28 -0500

    Add files via upload

---
+++ Firmware/sntp/build/CMakeFiles/3.16.4/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.19041")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.19041")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("C:/Users/Josue/ESP32/esp-idf/tools/cmake/toolchain-esp32s2.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
