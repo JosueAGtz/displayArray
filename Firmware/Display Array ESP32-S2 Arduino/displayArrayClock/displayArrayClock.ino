5386d9e370309e14c0ecc0f5f50a17fa78d47e30
Josue Alejandro Gutierrez <86976466+JosueAGtz@users.noreply.github.com>
2022-01-06 16:46:13 -0600

    Update displayArrayClock.ino

--- Firmware/Display Array ESP32-S2 Arduino/displayArrayClock/displayArrayClock.ino
+++ Firmware/Display Array ESP32-S2 Arduino/displayArrayClock/displayArrayClock.ino
@@ -1,10 +1,7 @@
-  
 #include <Adafruit_NeoPixel.h>
-
 #include <WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
-
 #include <bb_spi_lcd.h>
 
 #include "clockPixie.h"
@@ -41,8 +38,6 @@ NTPClient timeClient(ntpUDP);
 #define buffercounts    32
 
 Adafruit_NeoPixel pixel = Adafruit_NeoPixel(1, PINSLED, NEO_GRB + NEO_KHZ800);
-
-uint8_t ucTXBuf[1024];
 
 uint8_t imgBuffer[bufferLenght];
 
@@ -211,8 +206,6 @@ void setup() {
   digitalWrite(RESET_PIN, LOW);
   delay(100);
   digitalWrite(RESET_PIN, HIGH);
-
-  spilcdSetTXBuffer(ucTXBuf, sizeof(ucTXBuf));
 
   for (int i=0; i<6; i++) {
     spilcdInit(&lcd[i], LCD_ST7735S_B, FLAGS_INVERT | FLAGS_SWAP_RB, 40000000, CS1_PIN + i, DC_PIN, -1, LED_PIN, MISO_PIN, MOSI_PIN, SCK_PIN);
